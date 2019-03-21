549f6ad6cc2068b8602694228b08cc90fc83cc3c
priscillaX3 <43207475+priscillaX3@users.noreply.github.com>
2019-03-21 09:05:20 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".tinman").click(function() {
+    $(".heart").fadeIn();
+});
+
+$(".scarecrow").click(function() {
+    $(".brain").slideToggle();
+});
+
+$(".lion").click(function() {
+    $(".courage").fadeToggle();
+});
+
+$("button").click(function(){
+    $("img").show();
+});
